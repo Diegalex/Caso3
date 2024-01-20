a96fdb004e32d1e4bb388a6d9204f215eb3a3d92
diego <diego@LAPTOP-010Q2TEM>
2024-01-20 18:15:28 +0100

    area y perimetro triangulo

--- EntornosCaso3/src/Trabajo/Ejercicio.java
+++ EntornosCaso3/src/Trabajo/Ejercicio.java
@@ -4,18 +4,23 @@ public class Ejercicio {
 	public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        
         System.out.print("Ingresa la longitud del lado 1: ");
         double lado1 = scanner.nextDouble();
 
+        
         System.out.print("Ingresa la longitud del lado 2: ");
         double lado2 = scanner.nextDouble();
 
+        
         System.out.print("Ingresa la longitud del lado 3: ");
         double lado3 = scanner.nextDouble();
 
+        
         double perimetro = calcularPerimetro(lado1, lado2, lado3);
         System.out.println("Perímetro del triángulo: " + perimetro);
 
+        
         double area = calcularArea(lado1, lado2, lado3);
         System.out.println("Área del triángulo: " + area);
 
